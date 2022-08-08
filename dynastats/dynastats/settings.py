88cb4ed3f363b2fc2cdb5671143c40c69ad1b3e0
zach <40471358+orrefailaT@users.noreply.github.com>
2022-08-08 01:21:09 -0400

    decouple for configuration and secrets

--- dynastats/dynastats/settings.py
+++ dynastats/dynastats/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,10 +21,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-mu-%cw=oko5ue=ern&iin_f0ocno$uwa^u1ls9%*jx0fv-lj1e'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', cast=bool)
 
 ALLOWED_HOSTS = []
 
@@ -141,6 +142,6 @@ LOGOUT_REDIRECT_URL = "/"
 
 
 # Celery config
-CELERY_BROKER_URL = 'amqp://dynastats:MP3nAXpVFhjh8za5BBoc@localhost:5672/dynastats_vhost'
+CELERY_BROKER_URL = config('CELERY_BROKER_URL')
 CELERY_RESULT_BACKEND = 'django-db'
 CELERY_TASK_TRACK_STARTED = True
