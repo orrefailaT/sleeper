5b31bbf933faad25f11a7d5978b6214cbe2608c4
zach <40471358+orrefailaT@users.noreply.github.com>
2022-08-24 22:23:56 -0400

    un-hard coded urls

--- dynastats/main/urls.py
+++ dynastats/main/urls.py
@@ -6,7 +6,8 @@ from . import views
 app_name = 'main'
 urlpatterns = [
     path('', views.Index.as_view(), name='index'),
+    path('register/', views.Register.as_view(), name='register'),
     path('import/', views.Import.as_view(), name='import'),
     path('import/<str:task_id>/', views.ImportState.as_view(), name='import_state'),
-    path('register/', views.Register.as_view(), name='register'),
+    path('components/<str:component>/', views.Components.as_view(), name='components'),
 ]
